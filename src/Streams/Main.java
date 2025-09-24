828406c63dad1324c7fc340193df769033ee84f5
Cindy <210311822+clu1197@users.noreply.github.com>
2025-09-23 20:38:54 -0700

    new: streams

---
+++ src/Streams/Main.java
@@ -0,0 +1,129 @@
+package Streams;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize Bingo Pool
+        List<String> bingoPool = new ArrayList<>(75);
+        int start = 1;
+        for (char c : "BINGO".toCharArray()) {
+            for (int i = start; i < (start + 15); i++) {
+                bingoPool.add("" + c + i);
+                System.out.println("" + c + i);
+            }
+            start += 15;
+        }
+
+        // Shuffle and display first 15 elements
+        Collections.shuffle(bingoPool);
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("------------------------------------");
+
+        // Sort and format first 15 elements
+        List<String> firstOnes = new ArrayList<>(bingoPool.subList(0, 15));
+        firstOnes.sort(Comparator.naturalOrder());
+        firstOnes.replaceAll(s -> {
+            if (s.indexOf('G') == 0 || s.indexOf("O") == 0) {
+                String updated = s.charAt(0) + "-" + s.substring(1);
+                System.out.print(updated + " ");
+                return updated;
+            }
+            return s;
+        });
+        System.out.println("\n------------------------------------------");
+
+        // Display sorted and formatted elements
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+        }
+        System.out.println("------------------------------------");
+
+        // Stream operations on Bingo Pool
+        var tempStream = bingoPool.stream()
+                .limit(15)
+                .filter(s -> s.indexOf('G') == 0 || s.indexOf("O") == 0)
+                .map(s -> s.charAt(0) + "-" + s.substring(1))
+                .sorted();
+
+        tempStream.forEach(s -> System.out.println(s + " "));
+        System.out.println("------------------------------------");
+
+        // Stream operations on arrays and concat streams
+        String[] strings = {"One", "Two", "Three"};
+        var firstStream = Arrays.stream(strings)
+                .sorted(Comparator.reverseOrder());
+
+        var secondStream = Stream.of("Six", "Five", "Four")
+                .map(String::toUpperCase);
+
+        Stream.concat(secondStream, firstStream)
+                .map(s -> s.charAt(0) + " - " + s)
+                .forEach(System.out::println);
+
+        // Create and display a Bingo map
+        Map<Character, int[]> myMap = new LinkedHashMap<>();
+        int bingoIndex = 1;
+        for (char c : "BINGO".toCharArray()) {
+            int[] numbers = new int[15];
+            int labelNo = bingoIndex;
+            Arrays.setAll(numbers, i -> i + labelNo);
+            myMap.put(c, numbers);
+            bingoIndex += 15;
+        }
+
+        myMap.entrySet()
+                .stream()
+                .map(e -> e.getKey() + " has range: " + e.getValue()[0] + " - " +
+                        e.getValue()[e.getValue().length - 1])
+                .forEach(System.out::println);
+
+        // Generate random numbers
+        Random random = new Random();
+        Stream.generate(() -> random.nextInt(2))
+                .limit(10)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+
+        // Prime number streams
+        IntStream.iterate(1, n -> n + 1)
+                .filter(Main::isPrime)
+                .limit(20)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+
+        IntStream.iterate(1, n -> n <= 100, n -> n + 1)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+
+        IntStream.range(1, 100)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+
+        System.out.println();
+
+        IntStream.rangeClosed(1, 999)
+                .filter(Main::isPrime)
+                .forEach(s -> System.out.print(s + " "));
+    }
+
+    // Helper function to determine if a number is prime
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
